5efd0a85b7e127e7d0650bf3788177e43c8c319a
Pierrick Brun <pierrick.brun@akretion.com>
2021-10-06 20:02:16 +0200

    [IMP] allow to use multipart/form-data format

--- base_rest/http.py
+++ base_rest/http.py
@@ -121,7 +121,10 @@ class HttpRestRequest(HttpRequest):
             except ValueError as e:
                 msg = "Invalid JSON data: %s" % str(e)
                 _logger.info("%s: %s", self.httprequest.path, msg)
-                raise BadRequest(msg) from e
+                raise BadRequest(msg)
+        elif self.httprequest.mimetype == "multipart/form-data":
+            # Do not reassign self.params
+            pass
         else:
             # We reparse the query_string in order to handle data structure
             # more information on https://github.com/aventurella/pyquerystring
