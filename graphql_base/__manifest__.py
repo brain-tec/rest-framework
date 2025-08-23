0da3269891562db20df9194cf3d6ceadcf735814
Mohajiro <lavrovuriy@gmail.com>
2025-08-23 04:38:48 +0200

    [FIX] graphql_base: restore index and comments in manifest
    Add comments im manifest.
    Restore initial version of index.html to make ports easier.
    
    Task: 4927

--- graphql_base/__manifest__.py
+++ graphql_base/__manifest__.py
@@ -11,6 +11,9 @@
     "website": "https://github.com/OCA/rest-framework",
     "depends": ["base"],
     "data": ["views/graphiql.xml"],
+    # We place an upper bound on graphql-server because it has undergone a major
+    # change in 3.0.0b8 in Aug 2025. When graphql-server 3 has stabilized we'll need
+    # to update this module to make it compatible with it.
     "external_dependencies": {"python": ["graphene", "graphql-server<3.0.0b8"]},
     "development_status": "Production/Stable",
     "maintainers": ["sbidoul"],
